f84c445c3fed10a5d528d91481ee29c7bb4b5280
王大锤 <3490009712@qq.com>
2025-09-20 21:25:22 +0800

    feat:add test function

--- lru/lru_test.go
+++ lru/lru_test.go
@@ -1,6 +1,9 @@
 package lru
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type String string
 
@@ -19,3 +22,31 @@ func TestGet(t *testing.T) {
 	}
 }
 
+func TestRemoveOldest(t *testing.T) {
+	k1,k2,k3:="key1","key2","k3"
+	v1,v2,v3:="value1","value2","v3"
+	cap := len(k1+k2+v1+v2)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+	if _, ok := lru.Get(k1); ok || lru.Len() != 2 {
+		t.Fatalf("RemoveOldest key1 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string,0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+	lru := New(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("v2"))
+	lru.Add("k3", String("v3"))
+	lru.Add("k4", String("v4"))
+	expect := []string{"key1","k2"}
+	if !reflect.DeepEqual(keys, expect) {
+		t.Fatalf("OnEvicted keys = %v, want %v", keys, expect)
+	}
+}
